60156b9797f21431635152789d32b9d9db032230
AhmadAbdallah13 <a7mtoo133@gmail.com>
2021-04-08 13:52:50 +0300

    add form element to auth page, with validations on fields

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { LoadingController } from '@ionic/angular';
 import { AuthService } from './auth.service';
@@ -33,5 +34,9 @@ export class AuthPage implements OnInit {
           this.router.navigateByUrl('/places/tabs/discover');
         }, 1500);
       });
+  }
+
+  onSubmit(loginForm: NgForm) {
+    console.log(loginForm);
   }
 }
